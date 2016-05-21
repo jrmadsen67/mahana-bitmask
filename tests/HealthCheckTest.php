c0da29c736d218c40ae586f126c4b174e2b1fe4c
Jeff Madsen <jrmadsen67@gmail.com>
2016-05-21 02:37:10 +0000

    test setup

---
+++ tests/HealthCheckTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace jrmadsen67\MahanaBitmap\Test;
+
+class HealthCheckTest extends \PHPUnit_Framework_TestCase {
+	function test_assert_true_is_true() {
+		$this->assertTrue(true);
+	}
+}
